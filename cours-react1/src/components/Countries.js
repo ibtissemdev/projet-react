96e76f05e080d5da4021cc16dbb118a0fcf1d249
ibtissemdev <ibtissem.khiri@gmail.comgit config --global user.email ibtissem.khiri@gmail.com>
2022-03-31 16:36:06 +0200

    projet react

--- cours-react1/src/components/Countries.js
+++ cours-react1/src/components/Countries.js
@@ -24,17 +24,21 @@ const Countries = () => {
                 {radios.map((continent) => (
                     <li>
                         <input 
-                        type="radio" id={continent}
+                        type="radio" 
+                        id={continent}
                         name="continentRadio" 
+                        checked={continent === selectedRadio}
                         onChange={(e) =>setSelectedRadio(e.target.id)}
                         />
                         <label htmlFor={continent}>{continent}</label>
                     </li>
                 ))}
             </ul>
+            {selectedRadio && <button onClick={() =>setSelectedRadio("")}>Annuler la recherche</button> }
             <ul>
                 {data
                 .filter((country)=> country.continents[0].includes(selectedRadio))
+                .sort((a,b) => b.population - a.population)
                 .slice(0, rangeValue)
                 .map((country, index) => (
                     <Card key={index} country={country} />

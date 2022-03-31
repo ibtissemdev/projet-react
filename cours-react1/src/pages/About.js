70d98117786daebebb1fba88bc09e2c56231a47d
ibtissemdev <ibtissem.khiri@gmail.comgit config --global user.email ibtissem.khiri@gmail.com>
2022-03-31 16:07:39 +0200

    affichage des pays ok

---
+++ cours-react1/src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>A propos</h1>
+            <br />
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos, quidem esse explicabo vero quas doloribus totam qui corrupti maxime omnis molestiae accusantium fugit aspernatur nam hic consectetur tempora. Soluta fuga officiis itaque ducimus rerum possimus vitae eos! Cum laborum maxime animi. Est dignissimos veniam provident at minima quibusdam tempore ipsa totam voluptas? Est, labore ex excepturi fuga, alias beatae voluptatem, laboriosam quas dignissimos adipisci nulla voluptate maxime eaque aspernatur rem voluptas tenetur dolorem sunt quasi odio recusandae reiciendis error? Dolor ipsa, aliquid exercitationem ex sit magnam ipsam quia nobis reiciendis! Quasi voluptatibus animi molestias reiciendis ab doloribus placeat minima ipsa.</p>
+            <br />
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis recusandae dolore alias amet placeat? Natus harum molestiae cumque dicta iste doloribus voluptatibus consectetur. Magnam maiores voluptates molestias distinctio mollitia repellendus sunt perspiciatis, saepe cum eum molestiae aspernatur suscipit perferendis facilis quae a modi in sit, architecto reprehenderit? Praesentium magnam culpa vitae quidem doloribus. Perspiciatis explicabo reiciendis tempora harum minus iusto ducimus aliquid sunt, similique sed consequuntur, adipisci, illo saepe ea at eius exercitationem omnis. Eius dignissimos blanditiis, natus explicabo exercitationem quo a et quibusdam maiores. Repellendus assumenda laborum enim deleniti provident aut, corrupti tempore rem fugit sapiente esse pariatur adipisci tempora, at consequatur itaque molestias nulla molestiae culpa doloribus? Commodi aliquid, vitae, nam aliquam necessitatibus sunt est in, dignissimos a mollitia porro? Ad delectus sunt, magni sit quasi neque sapiente doloremque nemo blanditiis ab. Impedit, pariatur ipsum! Odit ducimus a, minima fugiat facere placeat at sed autem sapiente dolores quo, beatae aliquam perspiciatis eaque minus. Reiciendis similique minima sed nihil quia in labore deleniti cupiditate exercitationem, nam tenetur accusamus corporis consectetur aspernatur fuga? Accusantium cum consequuntur libero consequatur quisquam animi laborum, qui iste nam, ad nemo mollitia eligendi. Reiciendis, fuga? Dolor a nesciunt ratione ab eius rem molestiae minima quisquam?</p>
+        </div>
+    );
+};
+
+export default About;
